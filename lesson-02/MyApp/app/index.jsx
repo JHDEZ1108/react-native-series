fd33fc8c191e21de216f107a886798f86e88cbfd
Josué Hernández <joheca1998@outlook.com>
2025-03-13 20:56:01 -0600

    Working with List Views

--- lesson-02/MyApp/app/index.jsx
+++ lesson-02/MyApp/app/index.jsx
@@ -1,27 +1,36 @@
 import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
-import { Link } from 'expo-router';
-import React from 'react'
+import { Link } from 'expo-router'
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png"
 
 const app = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground 
-        source={require('@/assets/images/iced-coffee.png')}
-        resizeMode='cover'
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode="cover"
         style={styles.image}
       >
         <Text style={styles.title}>Coffee Shop</Text>
+
+        <Link href="/menu" style={{ marginHorizontal: 'auto' }} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Our Menu</Text>
+          </Pressable>
+        </Link>
+
         <Link href="/contact" style={{ marginHorizontal: 'auto' }} asChild>
           <Pressable style={styles.button}>
             <Text style={styles.buttonText}>Contact Us</Text>
           </Pressable>
         </Link>
+
       </ImageBackground>
     </View>
   )
 }
 
-export default app;
+export default app
 
 const styles = StyleSheet.create({
   container: {
@@ -54,10 +63,12 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     justifyContent: 'center',
     backgroundColor: 'rgba(0,0,0,0.75)',
     padding: 6,
+    marginBottom: 50,
   },
   buttonText: {
     color: 'white',
